5be10424adc2ea3f5e62054a65f53ac8c1443e5f
mrj <376531690@qq.com>
2021-01-19 16:21:45 +0800

    feat(fabric monitor):add fabric montor

--- fabric/tx/tx.go
+++ fabric/tx/tx.go
@@ -3,21 +3,26 @@ package tx
 import (
 	"github.com/ehousecy/notary-samples/notary-server/services"
 	pb "github.com/ehousecy/notary-samples/proto"
+	"github.com/hyperledger/fabric-protos-go/peer"
 )
 
 type Handler interface {
-	Handle(pb.NotaryService_ConstructTxServer, string) error
+	HandleOfflineTx(srv pb.NotaryService_ConstructTxServer, ticketId string) error
+	HandleLocalTx(ticketId string) error
+	HandleTxStatusBlock(channelID string, fb *peer.FilteredBlock)
 }
+
+var confirmingTxIDMap = make(map[string]map[string]byte, 8)
 
 type txHandler struct {
 	db services.CrossTxDataService
 }
 
-func New(db services.CrossTxDataService) *txHandler {
-	return &txHandler{db: db}
+func New() *txHandler {
+	return &txHandler{db: services.NewCrossTxDataServiceProvider()}
 }
 
-func (th *txHandler) offlineTxHandle(srv pb.NotaryService_ConstructTxServer, ticketId string) error {
+func (th *txHandler) HandleOfflineTx(srv pb.NotaryService_ConstructTxServer, ticketId string) error {
 	//通过ticketId查询跨链交易信息
 	//构造request
 	//获取fabric skd client对象
@@ -32,10 +37,38 @@ func (th *txHandler) offlineTxHandle(srv pb.NotaryService_ConstructTxServer, tic
 	return nil
 }
 
-func (th *txHandler) localTxHandle(ticketId string) error {
+func (th *txHandler) HandleLocalTx(ticketId string) error {
 	//通过ticketId查询跨链交易信息
 	//构造request
 	//获取fabric skd client对象
 	//发送交易
 	return nil
+}
+
+func (th *txHandler) HandleTxStatusBlock(channelID string, fb *peer.FilteredBlock) {
+	for _, ft := range fb.FilteredTransactions {
+		th.handleTx(channelID, ft)
+	}
+
+}
+
+func (th *txHandler) handleTx(channelID string, ft *peer.FilteredTransaction) bool {
+	//判断交易id是否有效
+	if ft.Txid == "" || ft.TxValidationCode != peer.TxValidationCode_VALID {
+		return true
+	}
+
+	//判断交易id是否需要处理
+	txMap, ok := confirmingTxIDMap[channelID]
+	if !ok || txMap == nil {
+		return true
+	}
+	if _, ok := txMap[ft.Txid]; !ok {
+		return true
+	}
+
+	//todo: 调用db完成交易
+	//处理完删除交易id
+	delete(txMap, ft.Txid)
+	return false
 }
