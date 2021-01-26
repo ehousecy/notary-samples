c27cd85fac8abf8e55941d678314f57d4a018036
mrj <376531690@qq.com>
2021-01-26 18:28:19 +0800

    fix(scripts):uncomment start-node.sh

--- scripts/start-node.sh
+++ scripts/start-node.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env sh
 
-# print(){
-#   echo "$1"
-# }
+print(){
+  echo "$1"
+}
 
-# print "starting Notary service ..."
-# print "starting ethereum node..."
-# print "initing geth data"
-# geth init genesis.json
-# #start geth background
-# echo "generating coinbase"
-# ./gen-accounts.sh
-# nohup geth --http --http.addr 127.0.0.1 --http.port 8545 --ws --ws.port 3334 --nodiscover &
-# print "geth started successfully..."
+print "starting Notary service ..."
+print "starting ethereum node..."
+print "initing geth data"
+geth init genesis.json
+#start geth background
+echo "generating coinbase"
+./gen-accounts.sh
+nohup geth --http --http.addr 127.0.0.1 --http.port 8545 --ws --ws.port 3334 --nodiscover &
+print "geth started successfully..."
 
-# until test -e ~/.ethereum/geth.ipc
-# do
-#     echo "waiting node initialize..."
-#     sleep 2
-# done
+until test -e ~/.ethereum/geth.ipc
+do
+    echo "waiting node initialize..."
+    sleep 2
+done
 
-# #genenrate coinbase account
-# #echo "generating coinbase"
-# #./gen-accounts.sh
-# ./auto-mining.sh
+#genenrate coinbase account
+#echo "generating coinbase"
+#./gen-accounts.sh
+./auto-mining.sh
 
-# echo "geth successfully started!"
+echo "geth successfully started!"
 
 
 
