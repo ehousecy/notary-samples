46af341f9e97573162538e09edde87c5266a3136
mrj <376531690@qq.com>
2021-01-29 17:23:31 +0800

    feat(fabric script):add error judgment

--- scripts/fabric/network-init.sh
+++ scripts/fabric/network-init.sh
@@ -12,8 +12,8 @@
 #
 # prepending $PWD/../bin to PATH to ensure we are picking up the correct binaries
 # this may be commented out to resolve installed version of tools if desired
-echo "====================== [process] init Fabric testnet data ========================="
-cd fabric-samples/test-network
+echo "====================== [process] init Fabric network data ========================="
+cd fabric-samples/test-network || exit
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 
@@ -24,8 +24,15 @@ export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.examp
 export CORE_PEER_ADDRESS=localhost:7051
 
 peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
-
+res=$?
+if [ $res -ne 0 ]; then
+    fatalln "Failed to init fabric chaincode data..."
+fi
 sleep 3
 peer chaincode query -C mychannel -n basic -c '{"Args":["GetAllAssets"]}'
+res=$?
+if [ $res -ne 0 ]; then
+    fatalln "Failed to query fabric chaincode data..."
+fi
 
-echo "====================== [finished] init Fabric testnet data ========================="
+echo "====================== [finished] init Fabric network data ========================="
